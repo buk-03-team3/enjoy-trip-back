d0c5a8b4a1bd45d15a792f0204ab90f8dc9629fe
이현수 <znight1020@naver.com>
2024-05-16 15:57:10 +0900

    feat: 사용자 인증 필터

---
+++ src/main/java/com/ssafy/enjoyTrip/common/filter/LoginCheckFilter.java
@@ -0,0 +1,42 @@
+package com.ssafy.enjoyTrip.common.filter;
+
+import com.ssafy.enjoyTrip.SessionConst;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginCheckFilter implements Filter {
+    private static final String[] whiteList = {"/", "/users", "/login", "/logout", "/css/*", "/ico.*"};
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        try {
+            if(isLoginCheckPath(requestURI)) {
+                HttpSession session = httpRequest.getSession(false);
+                if(session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null){
+                    // 로그인으로 redirect 나중에 바뀔 수 있음
+                    httpResponse.sendRedirect("/login?redirectURL=" + requestURI);
+                    return; // 여기가 중요, 미인증 사용자는 다음으로 진행하지 않고 끝
+                }
+            }
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e; // 예외 로깅 가능 하지만, 톰캣까지 예외를 보내주어야 함
+        }
+    }
+
+    private boolean isLoginCheckPath(String requestURI){
+        return !PatternMatchUtils.simpleMatch(whiteList, requestURI);
+    }
+
+}
---
+++ src/main/java/com/ssafy/enjoyTrip/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.ssafy.enjoyTrip.config;
+
+import com.ssafy.enjoyTrip.common.filter.LoginCheckFilter;
+import com.ssafy.enjoyTrip.common.interceptor.LogInterceptor;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
