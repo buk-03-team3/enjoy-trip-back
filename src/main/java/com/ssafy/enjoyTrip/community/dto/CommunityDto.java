d1b5dbd2aa6b46d36bd3db92e281505da8d44c9e
이현수 <znight1020@naver.com>
2024-05-21 10:34:13 +0900

    fix: CommunityDto field 수정 및 관련 쿼리 수정

--- src/main/java/com/ssafy/enjoyTrip/community/dto/CommunityDto.java
+++ src/main/java/com/ssafy/enjoyTrip/community/dto/CommunityDto.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Data
 public class CommunityDto {
     private int communityId;
+    private String category;
     private int userId;
     private String name;
     private String userProfileImageUrl;
